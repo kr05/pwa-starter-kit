e4d5542bfb0e3b69502c53e8479932f7b20c6e70
Kevin I Rivera <kevin.ivan05@gmail.com>
2019-07-23 01:15:19 -0700

    first attempt at replacing polymer-cli with rollup

--- gulpfile.js
+++ gulpfile.js
@@ -12,12 +12,48 @@ const gulp = require('gulp');
 const rename = require('gulp-rename');
 const replace = require('gulp-replace');
 const del = require('del');
+const workboxBuild = require('workbox-build');
+
+gulp.task('prpl-server:copy-files', () => {
+  return gulp.src([
+    'images/**',
+    'manifest.json',
+    'push-manifest.json',
+    'node_modules/regenerator-runtime/runtime.js',
+    'node_modules/systemjs/dist/s.min.js',
+    'node_modules/@webcomponents/webcomponentsjs/**'
+  ], {base: '.'})
+  .pipe(gulp.dest('build'));
+});
+
+gulp.task('prpl-server:replace-env', () => {
+  return gulp.src(['index.html'])
+    .pipe(replace('NODE_ENVIRONMENT', 'production'))
+    .pipe(gulp.dest('build'));
+});
 
 /**
  * Cleans the prpl-server build in the server directory.
  */
 gulp.task('prpl-server:clean', () => {
   return del('server/build');
+});
+
+gulp.task('prpl-server:service-worker', () => {
+  const swSrc = 'service-worker.js';
+  const swDest = 'build/service-worker.js';
+
+  return workboxBuild.injectManifest({
+    swSrc,
+    swDest,
+    // Other configuration options...
+    globDirectory: 'build',
+    globPatterns: [
+      '**/*.{html,js,css,ico,png,json}',
+    ]
+  }).then(({count, size}) => {
+    console.log(`Generated ${swDest}, which will precache ${count} files, totaling ${size} bytes.`);
+  });
 });
 
 /**
@@ -38,6 +74,9 @@ gulp.task('prpl-server:build', () => {
 });
 
 gulp.task('prpl-server', gulp.series(
+  'prpl-server:copy-files',
+  'prpl-server:replace-env',
   'prpl-server:clean',
+  'prpl-server:service-worker',
   'prpl-server:build'
 ));
